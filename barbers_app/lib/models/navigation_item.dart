f366d29c24eb10141d7feb66f22ade11bb35f26a
Loshan Sundaramoorthy <losh0001@gmail.com>
2023-12-17 18:59:34 +0100

    Worked on search bar, added focusNode, and cursor color, and changed the size

--- barbers_app/lib/models/navigation_item.dart
+++ barbers_app/lib/models/navigation_item.dart
@@ -1,3 +1,4 @@
+import 'package:barbers_app/models/barber_model.dart';
 import 'package:flutter/material.dart';
 
 class NavigationItem {
--- barbers_app/lib/screens/search_screen.dart
+++ barbers_app/lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -9,43 +10,69 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   bool isActive = false;
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Stack(
+      alignment: Alignment.centerRight,
       children: [
-        SizedBox(
-          height: 100,
-          width:  isActive ? MediaQuery.of(context).size.width - 100 : MediaQuery.of(context).size.width -20,
-          child:  TextField(
-            onTap: () {
-              setState(() {
-                isActive = true;
-                debugPrint(isActive.toString());
-              });
-            },
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(25),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                focusNode: _focusNode,
+                onTap: () {
+                  setState(() {
+                    isActive = true;
+                  });
+                },
+                style: const TextStyle(color: Colors.white),
+                cursorColor: const Color.fromARGB(255, 255, 98, 7),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 25),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 255, 98, 7)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 255, 98, 7)),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        if (isActive)
+          Positioned(
+            right: 10,
+            child: InkWell(
+              onTap: () {
+                _focusNode.unfocus();
+                SystemChannels.textInput.invokeMethod('TextInput.hide');
+                setState(() {
+                  isActive = false;
+                });
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
             ),
           ),
-        ),
-        if(isActive) InkWell(
-          onTap: () {
-            setState(() {
-            isActive = false;
-              
-            });
-          },
-          child: const Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Text('Cancel', style: TextStyle(color: Colors.white),),
-          ),
-        )
       ],
-
     );
   }
 }
